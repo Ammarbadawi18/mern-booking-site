2d4865b92e2a6bad394fb21f2946ab95048288da
ammarbadawi18 <ammarbadawi18@gmail.com>
2024-01-03 12:57:47 +0200

    input validations

--- backend/src/routes/Users.ts
+++ backend/src/routes/Users.ts
@@ -1,10 +1,24 @@
 import express , {Request,Response} from 'express';
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
+import {check, validationResult} from 'express-validator';
 
 const router = express.Router();
 
-router.post("/" , async(req:Request, res:Response) =>{
+router.post("/" ,[
+    check("firstName", "First name is required").isString(),
+    check("lastName","Last name is required").isString(),
+    check("email","Email is required").isEmail(),
+    check("password","Password must be above 6 characters").isLength({
+        min: 6
+    })
+], async(req:Request, res:Response) =>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message:errors.array()})
+    }
+    
     try {
         let user = await User.findOne({
             email: req.body.email
