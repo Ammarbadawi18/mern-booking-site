de3c81a0def068812a7babfdf25b736d56d5d1b1
ammarbadawi18 <ammarbadawi18@gmail.com>
2024-01-03 13:53:33 +0200

    Login API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/Users';
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/login",authRoutes)
 app.use("/register", userRoutes)
 
 app.listen(3000, () => {
